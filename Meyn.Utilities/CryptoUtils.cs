3b44e27281005dcf93e78f6d44fcb61450d15c76
Prashansa Meyn <pm@globalpay.dk>
2022-07-20 21:40:08 +0200

    first commit

---
+++ Meyn.Utilities/CryptoUtils.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Common.Utilities
+{
+	public static class CryptoUtils
+	{
+		private static readonly RandomNumberGenerator RandomNumberGenerator = RandomNumberGenerator.Create();
+
+		public static string GetRandomNumber(int numberOfDigits)
+		{
+			var bytes = new byte[numberOfDigits];
+			RandomNumberGenerator.GetBytes(bytes);
+			return string.Join(string.Empty, bytes.Select(b => b % 10));
+		}
+		public static string ComputeSha256Hash(string rawData)
+		{
+			// Create a SHA256   
+			using (var sha256Hash = SHA256.Create())
+			{
+				// ComputeHash - returns byte array  
+				byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+				// Convert byte array to a string   
+				StringBuilder builder = new();
+				for (int i = 0; i < bytes.Length; i++)
+				{
+					builder.Append(bytes[i].ToString("x2"));
+				}
+				return builder.ToString();
+			}
+		}
+	}
+}
---
+++ Meyn.Utilities/Utils.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Common.Utilities
+{
+	public static class Utils
+	{
+		public static string SubstituteTemplate(string template, Dictionary<string, string> args)
+		{
+			var result = string.IsNullOrWhiteSpace(template) ? string.Join("\n", args.Keys.Select(k => $"##{k}##")) : template;
+			foreach (var arg in args)
+			{
+				result = result.Replace("##" + arg.Key + "##", arg.Value);
+			}
+			return result;
+		}
+	}
+}
